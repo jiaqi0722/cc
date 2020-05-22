c8630abddd2cb311c65a31d670c5b41686bdf95e
jack-cc-av <2239730393@qq.com>
2020-05-22 09:57:03 +0800

    hh

--- src/main/java/com/cc/api/common/aop/RestAop.java
+++ src/main/java/com/cc/api/common/aop/RestAop.java
@@ -42,7 +42,7 @@ public class RestAop {
     public RestAop() {
     }
 
-    @Around("execution(public * com.myjob.api.controller..*.*(..))")
+    @Around("execution(public * com.cc.api.controller..*.*(..))")
     public Object AroundRest(ProceedingJoinPoint joinPoint) {
         String target = null;
         Object result = null;
